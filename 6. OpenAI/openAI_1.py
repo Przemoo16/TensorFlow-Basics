6c9706a524f17ebd1935af90f461dbd0032b2349
Przemoo16 <kanach16@gmail.com>
2019-02-12 14:32:32 +0100

    Add Word2Vec and OpenAI

---
+++ 6. OpenAI/openAI_1.py
@@ -0,0 +1,35 @@
+import gym
+
+env = gym.make('CartPole-v0')
+
+print("INITIAL OBSERVATION")
+observation = env.reset()
+print(observation)
+
+for i in range(2):
+    
+    #env.render()
+
+    action = env.action_space.sample()
+    
+    observation, reward, done, info = env.step(action)
+
+    print("Performed One Random Action")
+    print('\n')
+    print('observation')
+    print(observation)
+    print('\n')
+
+    print('reward')
+    print(reward)
+    print('\n')
+
+    print('done')
+    print(done)
+    print('\n')
+
+    print('info')
+    print(info)
+    print('\n')
+
+env.close()
---
+++ 6. OpenAI/openAI_2.py
@@ -0,0 +1,24 @@
+import gym
+import time
+
+env = gym.make('CartPole-v0')
+
+observation = env.reset()
+
+for i in range(1000):
+	time.sleep(0.01)
+	env.render()
+
+	cart_pos, cart_vel, pole_ang, ang_vel = observation
+
+	if pole_ang > 0:
+		action = 1
+	else:
+		action = 0
+
+	observation, reward, done, info = env.step(action)
+
+	# if done:
+	# 	break
+		
+env.close()
